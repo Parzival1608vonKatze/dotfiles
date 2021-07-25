74c664fdcd7e83030f45d0062d38dfe4a7e5d302
Parzival1608vonKatze <65299676+Parzival1608vonKatze@users.noreply.github.com>
2021-07-25 12:47:46 +0200

    i3wm
    .

--- .not-dot-files!/cp-configs.sh
+++ .not-dot-files!/cp-configs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#main configs
-cp -fr /home/juli/.config/* /home/juli/Documents/GitHub/dotfiles/.config/
-# vscodium
-cp -fr /home/juli/.vscode-oss/* /home/juli/Documents/GitHub/dotfiles/.config/.vscode-oss
+#i3wm
+cp -fr /home/juli/.config/i3/* /home/juli/Documents/GitHub/dotfiles/.config/i3/
 
 
 
