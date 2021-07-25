a82db2894b39199970f61ca5324a916651b045c9
Parzival1608vonKatze <65299676+Parzival1608vonKatze@users.noreply.github.com>
2021-07-25 12:44:06 +0200

    .
    .

--- .not-dot-files!/cp-configs.sh
+++ .not-dot-files!/cp-configs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#main configs
 cp -fr /home/juli/.config/* /home/juli/Documents/GitHub/dotfiles/.config/
+# vscodium
+cp -fr /home/juli/.vscode-oss/* /home/juli/Documents/GitHub/dotfiles/.config/.vscode-oss
+
+
 
 #                                 (END OF FILE)
