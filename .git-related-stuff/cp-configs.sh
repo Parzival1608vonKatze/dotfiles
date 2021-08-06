88394edfd3cc1638285a025c9a2747358f9484a4
Parzival1608vonKatze <65299676+Parzival1608vonKatze@users.noreply.github.com>
2021-08-06 18:32:16 +0200

    .

---
+++ .git-related-stuff/cp-configs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# this is a configuration file made by
+#       ______              _            _  My Website: https://parzival1608vonkatze.github.io/
+#       | ___ \            (_)          | | Mastadon: https://social.linux.pizza/@ParzivalvonKatze
+#       | |_/ /_ _ _ __ _______   ____ _| | Even more in a single link ;) : https://app.singlelink.co/u/parzival
+#       |  __/ _` | '__|_  / \ \ / / _` | |
+#       | | | (_| | |   / /| |\ V / (_| | |
+#       \_|  \__,_|_|  /___|_| \_/ \__,_|_|
+#                                   
+# Licenced under the newest version of the GNU GENERAL PUBLIC LICENSE
+
+
+
+#i3wm
+cp -fr ~/.config/i3/* ~/Documents/GitHub/dotfiles/.config/i3/
+
+# alacritty
+cp -fr ~/.config/alacritty.yml ~/Documents/GitHub/dotfiles/.config/
+
+# awesomewm
+cp -fr ~/.config/awesome/* ~/Documents/GitHub/dotfiles/.config/awesome/
+
+# picom / compton
+cp -fr ~/.config/picom/picom.conf ~/Documents/GitHub/dotfiles/.config/picom/
+
+
+#                                 (END OF FILE)
--- .git-related-stuff/desktop-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#i3wm
-cp -fr /home/juli/.config/i3/* /home/juli/Documents/GitHub/dotfiles/desktop/.config/i3/
-
-#                                 (END OF FILE)
